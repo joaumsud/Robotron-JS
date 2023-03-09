1a3a27ce529314df4199fa448cc85550a770f491
lsijoa02 <joaopaulo.lsilva@csgi.com>
2023-03-09 15:13:05 -0300

    adicionando data-contador

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach(  (elemento)=> {
     elemento.addEventListener("click", (evento) =>{
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
     })
  });
 
 function manipulaDados(operacao, controle){
-    const peca = controle.querySelector(".controle-contador");
+    const peca = controle.querySelector("[data-contador]");
     if(operacao === "-"){
         peca.value = parseInt(peca.value) - 1;
         if (peca.value <= 0)  peca.value = 0;
