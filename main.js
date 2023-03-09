87a7eb4e4844696c6e6fc963525057a254ecc18d
lsijoa02 <joaopaulo.lsilva@csgi.com>
2023-03-09 15:02:05 -0300

    adicionando o data-attribute - data-controle

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach(  (elemento)=> {
     elemento.addEventListener("click", (evento) =>{
