a35c63fd34105839a7244d1cb590b26e0d757c0a
lsijoa02 <joaopaulo.lsilva@csgi.com>
2023-03-09 14:57:38 -0300

    buscando elemento pelo parentNode

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
 const controle = document.querySelectorAll(".controle-ajuste");
-
-
 
 controle.forEach(  (elemento)=> {
     elemento.addEventListener("click", (evento) =>{
-        manipulaDados(evento.target.textContent);
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
     })
  });
 
-function manipulaDados(operacao){
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector(".controle-contador");
     if(operacao === "-"){
-        braco.value = parseInt(braco.value) - 1;
-        if (braco.value <= 0)  braco.value = 0;
+        peca.value = parseInt(peca.value) - 1;
+        if (peca.value <= 0)  peca.value = 0;
     }else {
-        braco.value = parseInt(braco.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 }
 
